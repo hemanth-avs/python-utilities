4d725406994bf3b943065f3835d4e4f0ffb85309
hemanth.avs <hemanth.avs@us.ibm.com>
2018-07-14 12:38:25 -0500

    Added Insertion Sort

---
+++ algorithms/sort/insertion_sort.py
@@ -0,0 +1,13 @@
+"""
+    Insertion Sort
+"""
+
+
+def sort(collection):
+
+    for outer_ind in range(1, len(collection)):
+        for inner_idx in range(outer_ind, 0, -1):
+            if collection[inner_idx] < collection[inner_idx-1]:
+                collection[inner_idx], collection[inner_idx - 1] = collection[inner_idx - 1], collection[inner_idx]
+            else:
+                break
