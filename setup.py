875746f233d3a75974f0cab9ad22f0a9a8cd7830
hemanth.avs <hemanth.avs@us.ibm.com>
2018-07-14 02:02:29 -0500

    Added Setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(name='python-utilities',
+      version='0.1.0',
+      description='Common Utility Helpers, Data structures and algorithms in Python',
+      long_description='Common Utility Helpers, Data structures and algorithms in Python',
+      url='https://github.com/keon/algorithms',
+      author='Hemanth AVS',
+      license='Apache License 2.0',
+      packages=find_packages(),
+      classifiers=[
+          'Programming Language :: Python :: 3.6',
+          ],
+      zip_safe=False)
